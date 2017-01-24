706114c8f99c36a91fa28e028007eea0750a3620
myeongjin <myeongjin0@naver.com>
2017-01-24 19:34:09 +0900

    divide lee and chan

---
+++ Lee/Braille_learning/app/src/main/java/com/example/yeo/practice/Common_quiz_sound/writing_final_service.java
@@ -0,0 +1,16 @@
+package com.example.yeo.practice.Common_quiz_sound;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class writing_final_service extends Service {
+    public writing_final_service() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
